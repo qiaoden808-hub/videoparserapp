77c02b0c5d48f87ac4490ccfd4af33fb079f9d11
LAPTOP-THCJL7SJ\47666 <476669806@qq.com>
2026-05-05 13:30:40 +0800

    fix: pre-cache assets in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,11 @@
-self.addEventListener('install', () => self.skipWaiting());
+const CACHE = 'video-parser-v1';
+const ASSETS = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(clients.claim());
@@ -9,7 +16,7 @@ self.addEventListener('fetch', (event) => {
     caches.match(event.request).then((cached) => {
       return cached || fetch(event.request).then((response) => {
         if (response.status === 200 && event.request.method === 'GET') {
-          const cache = caches.open('video-parser-v1');
+          const cache = caches.open(CACHE);
           cache.then((c) => c.put(event.request, response.clone()));
         }
         return response;
